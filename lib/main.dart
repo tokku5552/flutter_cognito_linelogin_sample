77b962c6e11d4307a8c0c941ef10e1943cb979fa
tokku5552 <physics.0121@gmail.com>
2024-01-13 11:24:10 +0900

    fix: sign in and out with flutter_line_sdk

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_line_sdk/flutter_line_sdk.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
@@ -46,6 +47,8 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   bool _isLoading = false;
+  bool _isLoggedIn = false;
+  String _displayText = 'ログインしていません';
 
   Future<void> _signInWithSDK() async {
     setState(() {
@@ -53,17 +56,54 @@ class _HomePageState extends State<HomePage> {
     });
     try {
       // LineSDK の login メソッドをコールする。
-      final loginResult = await LineSDK.instance.login();
+      final result = await LineSDK.instance.login();
+      final userId = result.userProfile?.userId;
+      final displayName = result.userProfile?.displayName;
 
-      // 得られる LoginResult 型の値にアクセストークン文字列が入っている。
-      final accessToken =
-          loginResult.accessToken.data['access_token'] as String;
-      debugPrint(accessToken);
+      setState(() {
+        _displayText = 'userId: $userId\ndisplayName: $displayName';
+      });
     } finally {
       setState(() {
         _isLoading = false;
+        _isLoggedIn = true;
       });
     }
+  }
+
+  Future<void> _signOutWithSDK() async {
+    setState(() {
+      _isLoading = true;
+    });
+    try {
+      await LineSDK.instance.logout();
+      setState(() {
+        _displayText = 'ログインしていません';
+      });
+    } on PlatformException catch (e) {
+      debugPrint(e.message);
+    } finally {
+      setState(() {
+        _isLoading = false;
+        _isLoggedIn = false;
+      });
+    }
+  }
+
+  List<Widget> _buttons() {
+    return _isLoggedIn
+        ? [
+            ElevatedButton(
+              onPressed: _isLoading ? null : _signOutWithSDK,
+              child: const Text('Logout'),
+            )
+          ]
+        : [
+            ElevatedButton(
+              onPressed: _isLoading ? null : _signInWithSDK,
+              child: const Text('LINE Login with SDK'),
+            )
+          ];
   }
 
   @override
@@ -72,14 +112,11 @@ class _HomePageState extends State<HomePage> {
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
-          ElevatedButton(
-            onPressed: _isLoading ? null : _signInWithSDK,
-            child: const Text('LINE Login'),
+          Text(
+            _displayText,
+            style: Theme.of(context).textTheme.headlineMedium,
           ),
-          // Text(
-          //   '$_counter',
-          //   style: Theme.of(context).textTheme.headlineMedium,
-          // ),
+          ..._buttons(),
         ],
       ),
     );
