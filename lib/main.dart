43525ab3cdfcda53b514ab5819b9cee618f831d6
tokku5552 <physics.0121@gmail.com>
2024-01-13 10:36:56 +0900

    fix: channel id environment

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_line_sdk/flutter_line_sdk.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // .envファイルからLINE CHANNEL IDを読み込んでLINE SDKを初期化する
+  await dotenv.load(fileName: ".env");
+  final channelId = dotenv.get('LINE_CHANNEL_ID');
+  await LineSDK.instance.setup(channelId).then((_) {});
+
   runApp(const App());
 }
 
@@ -9,60 +18,69 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final theme = ThemeData(
+      colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      useMaterial3: true,
+    );
+
     return MaterialApp(
       title: 'LINE Login Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+      theme: theme,
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: theme.colorScheme.inversePrimary,
+          title: const Text('LINE Login Demo'),
+        ),
+        body: const HomePage(),
       ),
-      home: const HomePage(title: 'LINE Login Demo'),
     );
   }
 }
 
 class HomePage extends StatefulWidget {
-  const HomePage({super.key, required this.title});
-
-  final String title;
+  const HomePage({super.key});
 
   @override
   State<HomePage> createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
-  int _counter = 0;
+  bool _isLoading = false;
 
-  void _incrementCounter() {
+  Future<void> _signInWithSDK() async {
     setState(() {
-      _counter++;
+      _isLoading = true;
     });
+    try {
+      // LineSDK の login メソッドをコールする。
+      final loginResult = await LineSDK.instance.login();
+
+      // 得られる LoginResult 型の値にアクセストークン文字列が入っている。
+      final accessToken =
+          loginResult.accessToken.data['access_token'] as String;
+      debugPrint(accessToken);
+    } finally {
+      setState(() {
+        _isLoading = false;
+      });
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          ElevatedButton(
+            onPressed: _isLoading ? null : _signInWithSDK,
+            child: const Text('LINE Login'),
+          ),
+          // Text(
+          //   '$_counter',
+          //   style: Theme.of(context).textTheme.headlineMedium,
+          // ),
+        ],
       ),
     );
   }
